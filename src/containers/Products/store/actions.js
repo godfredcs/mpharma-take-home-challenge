aec003369c409b33537e235049df5e3399d426db
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2021-02-10 02:13:14 +0000

    Adding products store

--- src/containers/Products/store/actions.js
+++ src/containers/Products/store/actions.js
@@ -1,6 +1,8 @@
 import Service from './service';
 import * as types from './types';
 
+export const statusChanged = (payload) => ({ type: types.STATUS_CHANGED, payload });
+
 /**
  * Products Action Creator - Get All Products
  *
@@ -13,12 +15,78 @@ export const fetchAllProducts = (callback) => async dispatch => {
         const result = await Service.all();
 
         if (result && result.products) {
-            dispatch({ type: types.GET_ALL_PRODUCTS_SUCCESS, payload: result });
-
+            dispatch({ type: types.GET_ALL_PRODUCTS_SUCCESS, payload: result.products });
+            console.log(`${types.GET_ALL_PRODUCTS_SUCCESS}: `, result);
             callback && callback();
         }
     } catch (error) {
-        console.log(`${types.GET_ALL_PRODUCTS_FAIL}: ${error}`);
+        console.log(`${types.GET_ALL_PRODUCTS_FAIL}: `, error);
         dispatch({ type: types.GET_ALL_PRODUCTS_FAIL, payload: error });
+    }
+}
+
+/**
+ * Add a product
+ *
+ * @param {Object} data
+ * @param {Function} callback
+ */
+export const addProduct = (data, callback) => async dispatch => {
+    try {
+        dispatch({ type: types.ADD_PRODUCT_START });
+
+        if (!data.name || !data.price) {
+            return dispatch({ type: types.ADD_PRODUCT_FAIL });
+        }
+
+        dispatch({ type: types.ADD_PRODUCT_SUCCESS, payload: data });
+        callback && callback();
+    } catch (error) {
+        console.log('error from adding product: ', error);
+    }
+}
+
+/**
+ * Edit a product
+ *
+ * @param {Object} data
+ * @param {Function} callback
+ */
+export const editProduct = (data, callback) => async dispatch => {
+    try {
+        dispatch({ type: types.EDIT_PRODUCT, payload: data });
+        callback && callback();
+    } catch (error) {
+        console.log('error from editing product: ', error);
+    }
+}
+
+/**
+ * Delete a product
+ *
+ * @param {Number} id
+ * @param {Function} callback
+ */
+export const deleteProduct = (id, callback) => async dispatch => {
+    try {
+        dispatch({ type: types.DELETE_PRODUCT, payload: id })
+        callback && callback();
+    } catch (error) {
+        console.log('error while deleting product: ', error);
+    }
+}
+
+/**
+ * Restore a deleted product
+ *
+ * @param {Number} id
+ * @param {Function} callback
+ */
+export const restoreProduct = (id, callback) => async dispatch => {
+    try {
+        dispatch({ type: types.RESTORE_PRODUCT, payload: id });
+        callback && callback();
+    } catch (error) {
+        console.log('error while restoring product: ', error);
     }
 }
--- src/containers/Products/store/reducer.js
+++ src/containers/Products/store/reducer.js
@@ -2,11 +2,21 @@ import * as types from './types';
 
 const INITIAL_STATE = {
     products: [],
+    status: 'active',
+    product_added: false,
     fetching_all_products: false,
 };
 
+let index = null;
+
 const reducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case types.STATUS_CHANGED:
+            return {
+                ...state,
+                status: action.payload,
+            };
+
         case types.GET_ALL_PRODUCTS_START:
             return {
                 ...state,
@@ -14,10 +24,16 @@ const reducer = (state = INITIAL_STATE, action) => {
             };
 
         case types.GET_ALL_PRODUCTS_SUCCESS:
+            action.payload.forEach(product => {
+                if (!state.products.find(prod => prod.id === product.id)) {
+                    state.products.push(product);
+                }
+            });
+
             return {
                 ...state,
-                products: action.payload,
                 fetching_all_products: false,
+                products: [ ...state.products ],
             };
 
         case types.GET_ALL_PRODUCTS_FAIL:
@@ -25,6 +41,73 @@ const reducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 fetching_all_products: false,
             };
+
+        case types.ADD_PRODUCT_SUCCESS:
+            let new_product = {
+                id: state.products[state.products.length - 1].id + 1,
+                name: action.payload.name,
+                prices: [
+                    {
+                        id: 1,
+                        price: action.payload.price,
+                        date: new Date().toISOString()
+                    }
+                ]
+            };
+
+            return {
+                ...state,
+                product_added: true,
+                products: [
+                    ...state.products,
+                    new_product,
+                ],
+            };
+
+        case types.EDIT_PRODUCT:
+            index = state.products.findIndex(product => product.id === action.payload.id);
+
+            if (index && index < 0) {
+                return state;
+            }
+
+            if (action.payload.name) {
+                state.products[index].name = action.payload.name;
+            }
+
+            const last_price = state.products[index].prices[state.products[index].prices.length - 1];
+
+            if (Number(action.payload.price) && last_price.price !== Number(action.payload.price)) {
+                state.products[index].prices.push({
+                    id: last_price.id + 1,
+                    price: action.payload.price,
+                    date: new Date().toISOString(),
+                });
+            }
+
+            return { ...state, products: [...state.products] };
+
+        case types.DELETE_PRODUCT:
+            index = state.products.findIndex(product => product.id === action.payload);
+
+            if (index && index < 0) {
+                return state;
+            }
+
+            state.products[index].deleted_at = new Date().toISOString();
+
+            return {...state, products: [...state.products]};
+
+        case types.RESTORE_PRODUCT:
+            index = state.products.findIndex(product => product.id === action.payload);
+
+            if (index && index < 0) {
+                return state;
+            }
+
+            state.products[index].deleted_at = null;
+
+            return { ...state, products: [...state.products] };
 
         default:
             return state;
